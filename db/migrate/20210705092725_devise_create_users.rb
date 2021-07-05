27e021ee71a0e972320e09afac44dfc9b2037585
mitachan-1 <youko.mita1027@gmail.com>
2021-07-05 18:33:05 +0900

    カラムを設定しました。

--- db/migrate/20210705092725_devise_create_users.rb
+++ db/migrate/20210705092725_devise_create_users.rb
@@ -6,6 +6,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :member_type_id,    null: false
+      t.string :nickname,           null: false
+      t.string :name,               null: false
+      t.string :name_kana,          null: false
+      t.integer :gender_id,         null: false
+      t.string :postal_code,        null: false
+      t.integer :prefecture_id,     null: false
+      t.string :city,               null: false
+      t.string :building
+      t.string :phone_number,       null: false
 
       ## Recoverable
       t.string   :reset_password_token
