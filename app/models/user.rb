6e0bedc1b2157e584c024d9e0794990872f9a6d0
mitachan-1 <youko.mita1027@gmail.com>
2021-07-05 18:38:12 +0900

    バリデーションを設定しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :member_type_id
+    validates :nickname
+    validates :name
+    validates :name_kana
+    validates :gender_id
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :building
+    validates :phone_number
+  end
+  
 end
